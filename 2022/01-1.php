44f7356791841af5f23813c2c5624ffa61ed42e9
Jan Huisink <JanHuisink@users.noreply.github.com>
2022-12-02 08:42:56 +0100

    2022-01-1

---
+++ 2022/01-1.php
@@ -0,0 +1,17 @@
+<?php
+require_once("lib.inc.php");
+
+$data = loadFile("01-1_input.txt");
+
+$elf[0] = 0;
+$elfid = 0;
+
+foreach($data AS $value) {
+    $value = trim($value);
+    if($value == "") {
+        $elf[++$elfid] = 0;
+    } else {
+        $elf[$elfid] += intval($value);
+    }
+}
+echo max($elf);
---
+++ 2022/lib.inc.php
@@ -0,0 +1,4 @@
+<?php
+function loadFile($filename) {
+    return file($filename);
+}
